9e1ea35f62fcccf3e95cc7a2e1b898310f4fd74f
rmarszolek <61089076+rmarszolek@users.noreply.github.com>
2021-02-13 22:31:45 +0100

    Add files via upload

---
+++ azure-vmubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo echo "Execute your super awesome commands here!" >> /tmp/test.txt
+sudo apt install openjdk-8-jdk -y
+sudo wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update && apt-get install jenkins -y
+sudo apt install openjdk-8-jdk -y
+sudo service jenkins restart
