c1a91d7321acb1f4f2b335e631e575a5618b82cb
Robert Marszołek <robert.marszolek400@outlook.com>
2021-04-02 18:39:55 +0000

    Deleted backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "devops104"
-    container_name       = "tfstate"
-    key                  = "jenkins-1.tfstate"
-    resource_group_name  = "devops"
-  }
-}
